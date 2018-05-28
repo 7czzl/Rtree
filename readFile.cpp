6662136d878d3d7f7f3e4ff4c2714c33ee5b7026
Potato <1025599465@qq.com>
2018-05-28 12:26:58 +0800

    Add files via upload

--- readFile.cpp
+++ readFile.cpp
@@ -5,7 +5,7 @@
 #include"readFile.h"
 using namespace std;
 
-void readFeature(string filename, vector<vector<int>>& Array)
+void readFeature(string filename, vector<vector<double>>& Array)
 {
 	ifstream inFile(filename);
 	if (!inFile.is_open())
@@ -14,28 +14,43 @@ void readFeature(string filename, vector<vector<int>>& Array)
 		return;
 	}
 	string lineArray;
-	vector<int> tempArray;
-	getline(inFile, lineArray, 't');
+	vector<double> tempArray;
+	getline(inFile, lineArray,'t');
 	getline(inFile, lineArray, 't');
 	inFile.ignore();
-	while (getline(inFile, lineArray))
+	//getline(inFile, lineArray);
+	while (getline(inFile, lineArray,'r'))
 	{
 		stringstream numbersTemp;
 		numbersTemp << lineArray;
 		string str;
 		int number = 0;
-		while (getline(numbersTemp, str, ' '))
+		while (getline(numbersTemp, str,' '))
 		{
 			stringstream numberTemp;
 			numberTemp << str;
 			numberTemp >> number;
 			tempArray.push_back(number);
 		}
+		int max = 0;
+		int min = 0;
+		for (int i = 0; i < tempArray.size(); i++)                       //¹éÒ»»¯
+		{
+			if (tempArray[i] > max)
+				max = tempArray[i];
+			if (tempArray[i] < min)
+				min = tempArray[i];
+		}
+		for (int i = 0; i < tempArray.size(); i++)
+		{
+			if(max != 0)
+				tempArray[i] = (tempArray[i] - min) / (double)(max - min);
+		}
 		Array.push_back(tempArray);
 		tempArray.clear();
 		inFile.ignore(5);
 	}
-	Array.pop_back();
+
 	inFile.close();
 }
 
--- readFile.h
+++ readFile.h
@@ -2,5 +2,5 @@
 #include<iostream>
 #include<vector>
 
-extern void readFeature(std::string filename, std::vector<std::vector<int>> &Array);
+extern void readFeature(std::string filename, std::vector<std::vector<double>> &Array);
 extern void readImage(std::string filename,std::vector <std::string> &Array);
